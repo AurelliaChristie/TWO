32c4692e34158d7474a738f6d0637cbaa899bd80
AurelliaChristie <aurelliachristie77@gmail.com>
2022-01-23 02:37:07 +0700

    Add DM first logic without socket

--- two/client/src/components/SideBar/SideBar.jsx
+++ two/client/src/components/SideBar/SideBar.jsx
@@ -1,15 +1,58 @@
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import axios from "axios";
+
+import { AuthContext } from "../../contexts/AuthContext";
 import User from "../User/User";
 import profile from "../../assets/default_profile.jfif";
 
 import "./SideBar.css";
 
-import { Users } from "../../dummyData";
-
 const SideBar = () => {
   const public_folder = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  const [conversations, setConversations] = useState([]);
+  const [allProfiles, setAllProfiles] = useState([]);
+
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getAllProfile = async () => {
+      try{
+        const profiles = await axios.get(`http://localhost:8000/users/${user.loggedIn._id}/all`);
+        setAllProfiles(profiles.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getAllProfile();
+
+    // const getUserConversations = async () => {
+    //   try{
+    //     const conversations = await axios.get(`http://localhost:8000/conversations/${user.loggedIn._id}`);
+    //     setConversations(conversations.data);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // }
+    // getUserConversations();
+
+    // const getOldConv = () => {
+    //   conversations.map((conv) => 
+    //     setOldConv(
+    //       oldConv.append(
+    //         conv.members.find(
+    //           (member) => member !== user.loggedIn._id
+    //         )
+    //       )
+    //     )
+    //   )        
+    // };
+    // getOldConv();
+
+  }, [user.loggedIn._id]);
 
   return(
     <div className="sidebar">
@@ -30,11 +73,15 @@ const SideBar = () => {
             <span className="sidebarListItemText ms-3">Direct Messages</span>
           </li>
           <ul className="sidebarUserList">
-            {Users.map((user) => (
-              <Link to={`/dm/${user._id}`} className="text-decoration-none text-white"  key={user._id}>
-                <User user={user}/>
-              </Link>
-            ))}
+            {allProfiles?.map((profile) => {
+                  return(
+                    <Link to={`/chat/${profile._id}`} className="text-decoration-none text-white"  key={profile._id}>
+                      <User user={profile}/>
+                    </Link>
+                  )
+                } 
+              )
+            }
           </ul>
         </ul>
       </div>
